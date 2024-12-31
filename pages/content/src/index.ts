743ac604e5293f3ab957c85e1e096af122731513
Ion Leu <ion.leu@gmail.com>
2024-12-30 19:31:44 -0500

    feature(content): add capture screenshot logic

--- pages/content/src/index.ts
+++ pages/content/src/index.ts
@@ -1,5 +1,17 @@
-import { toggleTheme } from '@src/toggleTheme';
+import { startScreenshotCapture } from './screenshot';
 
 console.log('content script loaded');
 
-void toggleTheme();
+// Listen for runtime messages
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  console.log('On Message Listener: ', msg);
+
+  if (msg.action === 'START_SCREENSHOT') {
+    // Call the function to initiate the screenshot capture process
+    startScreenshotCapture();
+  }
+
+  if (msg.action === 'CLOSE_MODAL') {
+    window.dispatchEvent(new CustomEvent('CLOSE_MODAL'));
+  }
+});
