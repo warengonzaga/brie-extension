026bb6b6d1ebc1dbceba999ade81b6dcaa0c81cc
Ion Leu <ion.leu@gmail.com>
2024-12-30 19:32:46 -0500

    chore: commit unsaved files

--- pages/side-panel/src/SidePanel.tsx
+++ pages/side-panel/src/SidePanel.tsx
@@ -1,14 +1,13 @@
 import '@src/SidePanel.css';
 import { useStorage, withErrorBoundary, withSuspense } from '@extension/shared';
-import { exampleThemeStorage } from '@extension/storage';
+import { themeStorage } from '@extension/storage';
 import type { ComponentPropsWithoutRef } from 'react';
 
 const SidePanel = () => {
-  const theme = useStorage(exampleThemeStorage);
+  const theme = useStorage(themeStorage);
   const isLight = theme === 'light';
   const logo = isLight ? 'side-panel/logo_vertical.svg' : 'side-panel/logo_vertical_dark.svg';
-  const goGithubSite = () =>
-    chrome.tabs.create({ url: 'https://briehq.com' });
+  const goGithubSite = () => chrome.tabs.create({ url: 'https://briehq.com' });
 
   return (
     <div className={`App ${isLight ? 'bg-slate-50' : 'bg-gray-800'}`}>
@@ -26,7 +25,7 @@ const SidePanel = () => {
 };
 
 const ToggleButton = (props: ComponentPropsWithoutRef<'button'>) => {
-  const theme = useStorage(exampleThemeStorage);
+  const theme = useStorage(themeStorage);
   return (
     <button
       className={
@@ -35,7 +34,7 @@ const ToggleButton = (props: ComponentPropsWithoutRef<'button'>) => {
         'font-bold mt-4 py-1 px-4 rounded shadow hover:scale-105 ' +
         (theme === 'light' ? 'bg-white text-black' : 'bg-black text-white')
       }
-      onClick={exampleThemeStorage.toggle}>
+      onClick={themeStorage.toggle}>
       {props.children}
     </button>
   );
