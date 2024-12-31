006eb87d764b03273cd330cfb60f145df14d1281
Ion Leu <ion.leu@gmail.com>
2024-12-30 19:24:31 -0500

    feature(extension): add backgoround listeners

--- chrome-extension/src/background/index.ts
+++ chrome-extension/src/background/index.ts
@@ -1,9 +1,46 @@
 import 'webextension-polyfill';
-import { exampleThemeStorage } from '@extension/storage';
 
-exampleThemeStorage.get().then(theme => {
-  console.log('theme', theme);
-});
+import { annotationsRedoStorage, annotationsStorage, captureStateStorage, captureTabStorage } from '@extension/storage';
 
 console.log('background loaded');
 console.log("Edit 'chrome-extension/src/background/index.ts' and save to reload.");
+
+chrome.tabs.onRemoved.addListener(async tabId => {
+  console.log('On Removed Listener: ', tabId);
+
+  const captureTabId = await captureTabStorage.getCaptureTabId();
+  if (tabId === captureTabId) {
+    await captureStateStorage.setCaptureState('idle');
+    await captureTabStorage.setCaptureTabId(null);
+
+    annotationsStorage.setAnnotations([]);
+    annotationsRedoStorage.setAnnotations([]);
+  }
+});
+
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo) => {
+  console.log('On onUpdated Listener: ', changeInfo.status);
+
+  if (changeInfo.status !== 'loading') return;
+
+  const captureTabId = await captureTabStorage.getCaptureTabId();
+  if (tabId === captureTabId) {
+    await captureStateStorage.setCaptureState('idle');
+    await captureTabStorage.setCaptureTabId(null);
+
+    annotationsStorage.setAnnotations([]);
+    annotationsRedoStorage.setAnnotations([]);
+  }
+});
+
+chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
+  console.log('Message received in background:', message);
+  if (message.type === 'EXIT_CAPTURE') {
+    await captureStateStorage.setCaptureState('idle');
+    await captureTabStorage.setCaptureTabId(null);
+
+    annotationsStorage.setAnnotations([]);
+    annotationsRedoStorage.setAnnotations([]);
+  }
+  sendResponse({ status: 'Message received' });
+});
