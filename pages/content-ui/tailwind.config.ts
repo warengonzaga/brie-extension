beb7a74983b28a95ecc6f496fbffee7d868b7f4d
Ion Leu <ion.leu@gmail.com>
2025-04-24 17:06:57 -0400

    bug(content): handle missing content-ui entry to prevent HMR failure and ENOENT error

--- pages/content-ui/tailwind.config.ts
+++ pages/content-ui/tailwind.config.ts
@@ -1,10 +1,5 @@
-import baseConfig from '@extension/tailwindcss-config';
 import { withUI } from '@extension/ui';
 
 export default withUI({
-  ...baseConfig,
-  /**
-   *  @todo test if needed and remove
-   */
-  content: ['src/**/*.tsx'],
+  content: ['./src/**/*.{ts,tsx}'],
 });
