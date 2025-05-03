9267bd379ab3dae3dcc96cdb5a460bee46dbec51
Luminita Leu <luminitaleu44@gmail.com>
2025-05-03 13:06:46 -0400

    feature(134): replace loading text with skeleton component

--- pages/popup/src/popup-content.tsx
+++ pages/popup/src/popup-content.tsx
@@ -2,12 +2,11 @@ import { useEffect, useState } from 'react';
 
 import { SlicesHistoryButton, SlicesHistoryContent } from './components/slices-history';
 import { CaptureScreenshotGroup } from './components/capture';
-import { Header, BetaNotifier } from './components/ui';
+import { Header, BetaNotifier, Skeleton } from './components/ui';
 
 import { authTokensStorage, captureStateStorage, userUUIDStorage } from '@extension/storage';
 import { useLoginGuestMutation } from '@extension/store';
 import { useStorage } from '@extension/shared';
-import { Skeleton } from '@extension/ui';
 
 export const PopupContent = () => {
   const captureState = useStorage(captureStateStorage);
@@ -31,27 +30,7 @@ export const PopupContent = () => {
 
   if (isLoading) {
     return (
-      <div className="relative mt-3">
-        <div className="items-center space-y-6 md:flex md:justify-between md:space-x-4 md:space-y-0">
-          <div className="flex justify-between">
-            <Skeleton className="h-8 w-[120px]" />
-
-            <div className="flex space-x-2">
-              <Skeleton className="size-8" />
-              <Skeleton className="size-8" />
-            </div>
-          </div>
-
-          <div className="grid space-y-3">
-            <Skeleton className="size-9 w-full rounded-lg" />
-
-            <div className="space-y-1.5">
-              <Skeleton className="m-auto h-3 w-[180px]" />
-              <Skeleton className="m-auto h-3 w-[250px]" />
-            </div>
-          </div>
-        </div>
-      </div>
+      <Skeleton />
     );
   }
 
