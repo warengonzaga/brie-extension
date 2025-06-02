46193e8a68a666a110d3de2a8e8e77310da9f99b
Ion Leu <ion.leu@gmail.com>
2025-06-01 20:54:02 -0400

    bug: redact sensitive info for every domain outside of localhost

--- packages/shared/lib/constants/non-production-keywords.constants.ts
+++ packages/shared/lib/constants/non-production-keywords.constants.ts
@@ -1,5 +1,11 @@
 /**
- * @todo
+ * @todo #93
  * allow user to decide which env is eligible for redacting sensitive info
  */
-export const nonProductionKeywords = ['uat.', 'test.', 'sandbox.', 'dev.', 'localhost'];
+export const nonProductionKeywords = [
+  // 'uat.',
+  // 'test.',
+  // 'sandbox.',
+  // 'dev.',
+  'localhost',
+];
